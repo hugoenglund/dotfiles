0e098135140d3b4715208edcfc50d0cdaaa627f5
Hugo Englund <hugo.englund@nepa.com>
2023-07-06 11:37:47 +0200

    feat: make lualine fire

--- .config/nvim/lua/huen/plugins/lualine.lua
+++ .config/nvim/lua/huen/plugins/lualine.lua
@@ -1,5 +1,22 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = "nvim-tree/nvim-web-devicons",
-	config = true,
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_b = {
+					"branch",
+					"diff",
+				},
+				lualine_c = {
+					"filename",
+					{
+						"diagnostics",
+						sources = { "nvim_lsp" },
+						symbols = { error = " ", warn = " ", info = " ", hint = " " },
+					},
+				},
+			},
+		})
+	end,
 }
